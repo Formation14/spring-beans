e2eae1f4d15feacc384d00870c2f93361913462e
Formation14 <paveltis@tut.by>
2021-06-09 11:02:57 +0300

    ver1.2

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -2,7 +2,6 @@ package app;
 
 import app.config.AppConfig;
 import app.model.AnimalsCage;
-import app.model.Timer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -15,6 +14,7 @@ public class Application {
             AnimalsCage bean =
                     applicationContext.getBean(AnimalsCage.class);
             bean.whatAnimalSay();
+            System.out.println(bean.getTimer().getTime());
         }
 
     }
--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,9 +1,6 @@
 package app.model;
 
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
-
 
 @Component
 public class Timer {
